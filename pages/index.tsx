b36520ef02ea5b52bd8f5a479d0ab6b51f8c9c33
jagnani73 <yashjagnani73@gmail.com>
2023-04-21 11:22:22 +0530

    fix: copy+mobile view height

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from "next";
+import { useCallback, useEffect } from "react";
 
 import { HomeScreen } from "#/components/home";
 import { DevNotes, Navbar } from "#/components/shared";
 
 const HomePage: NextPage = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setFullHeight();
+      window.addEventListener("resize", () => setFullHeight());
+    }
+  });
+
+  const setFullHeight = useCallback(
+    () =>
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      ),
+    []
+  );
+
   return (
     <section className="relative flex h-full w-full flex-col">
       <Navbar />
